b4152faf2db99d7c1d8c9169ecc9638bbeefaafa
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-02-26 01:44:32 +0100

    L'esercizio "Dadi"

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ if (boxEmail.includes(userEmail)) {
     alert('Purtroppo la tua email non esiste nel nostro archivio!');
 }
 
+/* Dadi */
+let buttonRandom = document.querySelector('button');
+buttonRandom.addEventListener('click', function() {
+    let userNumber = Math.floor(Math.random() * 7);
+    document.getElementById('user').innerHTML += userNumber;
+    console.log(userNumber);
+
+    let computerNumber = Math.floor(Math.random() * 7);
+    document.getElementById('computer').innerHTML += computerNumber;
+    console.log(computerNumber);
+
+    if (userNumber > computerNumber) {
+        alert('Hai vinto tu!');
+    } else if (computerNumber > userNumber) {
+        alert('Ho vinto io!');
+    } else {
+        alert('..click me ancora..');
+    }
+}
+);
